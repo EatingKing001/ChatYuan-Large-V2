7e7d674adda18754414a31854ddd9137dd8bfc01
jianxinDong1101 <104565194+jianxinDong1101@users.noreply.github.com>
2023-04-12 16:46:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -260,4 +260,4 @@ Based on the original functions of Chatyuan-large-v1, we optimized the model as 
 gui = gr.TabbedInterface(
     interface_list=[introduction, demo, demo_1],
     tab_names=["相关介绍 | Introduction", "开源模型 | Online Demo", "API调用"])
-gui.launch(quiet=True, show_api=False, share=False)
+gui.queue().launch()
